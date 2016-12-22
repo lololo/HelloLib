9b8b9ebd617728862a2f0527a175d2fbe7a0dc13
Ray <guolei912@gmail.com>
2016-12-22 11:44:21 +0800

    haha

---
+++ HelloLib/Classes/Hi.h
@@ -0,0 +1,13 @@
+//
+//  Hi.h
+//  Pods
+//
+//  Created by lei on 16/12/2016.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Hi : NSObject
++ (void)hi;
+@end
---
+++ HelloLib/Classes/Hi.m
@@ -0,0 +1,15 @@
+//
+//  Hi.m
+//  Pods
+//
+//  Created by lei on 16/12/2016.
+//
+//
+
+#import "Hi.h"
+
+@implementation Hi
++ (void)hi {
+    NSLog(@"Hello");
+}
+@end
